f57a2ba89221979396e04d25d801f5403e196048
Vadim Sevruk <v.sevrukas@gmail.com>
2018-11-16 08:19:28 +0200

    normal site

---
+++ script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+      // autoplay: true,
+      nav: true,
+      loop: true,
+      smartSpeed: 2000,
+      navText: ["<img src='/img/left.svg'>","<img src='/img/right.svg'>"],
+
+      responsive: {
+        320: {
+          items: 1,
+          nav: false,
+          stagePadding: 30
+        },
+        425: {
+          items: 1,
+          stagePadding: 80
+        },
+        726: {
+          items: 3,
+          stagePadding: 60,
+        },
+        1024: {
+          items: 4,
+          stagePadding: 60
+        },
+        1440: {
+          items: 4,
+          stagePadding: 100
+        },
+        2560: {
+          items: 5,
+          stagePadding: 125,
+        }
+      }
+    });
+  });
+
+  // "&#8249;&#8249","&#8250;&#8250"
+
+  // ["<i class='fas fa-angle-double-left'></i>","<i class='fas fa-angle-double-right'></i>"]
